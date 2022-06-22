8d75d2be2a3a77b821452d1a5c34324681a31c74
ymgyt <yamaguchi7073xtt@gmail.com>
2022-06-22 19:58:58 +0900

    feat(retry): def retry_if signature

--- retry/src/lib.rs
+++ retry/src/lib.rs
@@ -4,8 +4,46 @@ pub async fn retry<T>(task: T) -> Result<T::Item, T::Error>
     where
         T: Task,
 {
-    let mut task = task;
-    task.call().await
+    retry_if(task, Always).await
+}
+
+pub async fn retry_if<T,C>(
+    task: T,
+    condition: C,
+)  -> Result<T::Item, T::Error>
+where
+    T: Task,
+    C: Condition<T::Error>,
+
+{
+    // RetryPolicy::default().retry_if(task,condition).await
+    todo!()
+}
+
+/// A type to determine if a failed Future should be retried
+/// A implementation is provided for `Fn(&Err) -> bool` allowing yout
+/// to use a simple closure or fn handles
+pub trait Condition<E> {
+   fn is_retryable(
+       &mut self,
+       error: &E,
+   ) -> bool;
+}
+
+struct Always;
+
+impl<E> Condition<E> for Always {
+    fn is_retryable(&mut self, _: &E) -> bool {
+        true
+    }
+}
+
+impl<F,E> Condition<E> for F
+where F: FnMut(&E) -> bool,
+{
+    fn is_retryable(&mut self, error: &E) -> bool {
+        self(error)
+    }
 }
 
 /// A unit of work to be retried
